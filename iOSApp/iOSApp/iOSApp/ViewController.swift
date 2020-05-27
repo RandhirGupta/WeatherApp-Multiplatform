c433e5d7e83c47a7bed285f4fc2093c8932fdaa6
Randhir Gupta <randhirgupta92@gmail.com>
2020-05-27 21:32:05 +0530

    feat : Added ios app module

---
+++ iOSApp/iOSApp/iOSApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSApp
+//
+//  Created by serman on 27/05/20.
+//  Copyright © 2020 Randhir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
